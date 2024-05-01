0f4d5c8165ac50d2a9064b1a4c8cb69cdec898cc
Jesus1102 <jesus60302@gmail.com>
2024-05-01 15:00:21 -0500

    Update

--- VacacionesPYC.cpp
+++ VacacionesPYC.cpp
@@ -16,6 +16,7 @@ class TioSobrio{
         
         Viaje viaje;
         queue <Viaje> colaViajes;
+        int dia1, mes1, anio1;
 
     public:
 
@@ -28,12 +29,17 @@ class TioSobrio{
             cout << "Ingrese el destino del viaje (0. para salir): " << endl;
             cin >> viaje.destino;
             
-            if(buscarDestino(viaje.destino)){
+            if (viaje.destino == "0"){
+                break;
+            }
+            else if(buscarDestino(viaje.destino) == true){
                 
                 cout << "El destino ya ha sido registrado" << endl;
+                cout << "Ingrese otro valido" << endl;
 
             }
             else{
+
 
                 
             }
@@ -58,4 +64,79 @@ class TioSobrio{
         return false; // si no esta en la cola
 
     }
+
+    bool buscarFecha(string fecha){
+
+        queue <Viaje> colaAux = colaViajes;
+        while(!colaAux.empty()){
+            Viaje viaje = colaAux.front();
+            if(viaje.fecha == fecha){
+                return true; // si esta en la cola
+            }
+            else{
+                colaAux.pop();
+            }
+            
+        }
+
+        return false; // si no esta en la cola
+
+    }
+
+
+
+    string anadirFechas(){
+
+        while(true){
+
+            cout << "Ingrese la fecha del viaje ((DD/MM/AAAA)  0. para salir): " << endl;
+            cin >> dia1 >> mes1 >> anio1;
+
+            if(fechaValida(dia1, mes1, anio1)){
+
+                string fecha = to_string(dia1) + "/" + to_string(mes1) + "/" + to_string(anio1);
+                
+                if(buscarDestino(viaje.destino) == true){
+                
+                    cout << "La fecha ya ha sido registrada" << endl;
+                    cout << "Ingrese otro valido" << endl;
+                
+                }
+                else{
+
+                    cout << "Fecha registada" << endl;
+                    return fecha;
+                }
+
+            }
+            else{
+                
+                cout << "Fecha no valida" << endl;
+                cout << "Ingrese otra fecha" << endl;
+            }
+
+        }
+
+    }
+
+    bool fechaValida(int dia, int mes, int anio){
+        
+        if (dia < 1 || dia > 31)
+        {
+            return false;
+        }
+        
+        if (mes < 1 || mes > 12)
+        {
+            return false;
+        }
+        
+        if (anio < 2023)
+        {
+            return false;
+        }
+        
+        return true;
+    }
+
 };
