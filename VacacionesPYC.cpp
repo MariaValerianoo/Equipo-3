ac5540a87948fae4f86d92504b1be7a13e714105
Jesus1102 <jesus60302@gmail.com>
2024-05-02 18:29:05 -0500

    Update VacacionesPYC.cpp

--- VacacionesPYC.cpp
+++ VacacionesPYC.cpp
@@ -8,7 +8,7 @@ struct Viaje{
     string destino;
     string miembro;
     string fecha;
-    string activcidades;
+    string actividades;
     string hora;
 };
 
@@ -21,12 +21,13 @@ class TioSobrio{
         queue <string> viajesGenerales;
         queue <string> fechasGenerales;
         queue <string> actividadesGenerales;
+        queue <string> horasGenerales;
         int dia1, mes1, anio1;
 
     public:
 
     void anadirDestinos(){
-    
+        
         while(true){
 
             cout << "Ingrese el nombre del miembro: " << endl;
@@ -43,18 +44,123 @@ class TioSobrio{
                 cout << "Ingrese otro valido" << endl;
 
             }
-            else{
+            else if(buscarDestino(viaje.destino) == false){
 
                 viaje.fecha = anadirFechas();
+                viaje.actividades = anadirActividades();
                 colaViajes.push(viaje);
                 viajesGenerales.push(viaje.destino);
                 fechasGenerales.push(viaje.fecha);
+                actividadesGenerales.push(viaje.actividades);
+                horasGenerales.push(viaje.hora);
                 cout << "Destino Registrado" << endl;
                 
             }
         }
     }
-        
+
+    string anadirFechas(){
+
+        while(true){
+
+            cout << "Ingrese la fecha del viaje ((DD/MM/AAAA)  0. para salir): " << endl;
+            cin >> dia1 >> mes1 >> anio1;
+
+            if(fechaValida(dia1, mes1, anio1)){
+
+                string fecha = to_string(dia1) + "/" + to_string(mes1) + "/" + to_string(anio1);
+                
+                if(buscarDestino(viaje.destino) == true){
+                
+                    cout << "La fecha ya ha sido registrada" << endl;
+                    cout << "Ingrese otro valido" << endl;
+                
+                }
+                else{
+
+                    cout << "Fecha registada" << endl;
+                    return fecha;
+                }
+
+            }
+            else{
+                
+                cout << "Fecha no valida" << endl;
+                cout << "Ingrese otra fecha" << endl;
+            }
+
+        }
+
+    }
+
+    string anadirActividades(){
+
+        int hora, minuto;
+
+        while(true){
+
+            cout << "Ingrese la hora de la actividad (HH:MM): " << endl;
+            cin >> hora >> minuto;
+
+            if(buscarActividad(viaje.fecha) == true){
+                
+                cout << "Ya hay actividades registradas en esas horas" << endl;
+                cout << "Ingrese otras validas" << endl;
+
+            }
+            else if(buscarActividad(viaje.fecha) == false){
+                
+                cout << "Actividades registradas" << endl;
+                return viaje.fecha;
+
+            }
+
+        }
+
+    }
+
+    string anadirActividades(){
+
+        while(true){
+
+            cout << "Ingrese las actividades del viaje : " << endl;
+            cin >> viaje.actividades;
+
+            if(buscarActividad(viaje.actividades) == true){
+                
+                cout << "Las actividades ya han sido registradas" << endl;
+                cout << "Ingrese otras validas" << endl;
+
+            }
+            else if(buscarActividad(viaje.actividades) == false){
+                
+                cout << "Actividades registradas" << endl;
+                return viaje.actividades;
+
+            }
+
+        }
+
+    }
+
+    bool buscarActividad(string actividad){
+
+        queue <Viaje> colaAux = colaViajes;
+        while(!colaAux.empty()){
+            Viaje viaje = colaAux.front();
+            if(viaje.actividades == actividad){
+                return true; // si esta en la cola
+            }
+            else{
+                colaAux.pop();
+            }
+            
+        }
+
+        return false; // si no esta en la cola
+
+    }
+
     bool buscarDestino(string destino){
 
         queue <Viaje> colaAux = colaViajes;
@@ -91,39 +197,7 @@ class TioSobrio{
 
     }
 
-    string anadirFechas(){
-
-        while(true){
-
-            cout << "Ingrese la fecha del viaje ((DD/MM/AAAA)  0. para salir): " << endl;
-            cin >> dia1 >> mes1 >> anio1;
-
-            if(fechaValida(dia1, mes1, anio1)){
-
-                string fecha = to_string(dia1) + "/" + to_string(mes1) + "/" + to_string(anio1);
-                
-                if(buscarDestino(viaje.destino) == true){
-                
-                    cout << "La fecha ya ha sido registrada" << endl;
-                    cout << "Ingrese otro valido" << endl;
-                
-                }
-                else{
-
-                    cout << "Fecha registada" << endl;
-                    return fecha;
-                }
-
-            }
-            else{
-                
-                cout << "Fecha no valida" << endl;
-                cout << "Ingrese otra fecha" << endl;
-            }
-
-        }
-
-    }
+    
 
     bool fechaValida(int dia, int mes, int anio){
         
