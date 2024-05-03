4b8492ab3f43bc0013ac4ccd76b3aa00d2db8bb5
G4br13la <71794842+G4br13la@users.noreply.github.com>
2024-05-03 14:53:00 -0500

    Update parcial.cpp

--- parcial 3/parcial.cpp
+++ parcial 3/parcial.cpp
@@ -6,14 +6,13 @@
 using namespace std;
 
 /*registre los miembros de la famila con la edad y ordenar del mas viejo al mas joven*/
-struct Miembros_familia
-{
-    string miembro_familia;
-    int edad;
-}
+// struct Miembros_familia
+// {
+//     string miembro_familia;
+//     int edad;
+// };
 
-void
-registrar_miembros(string familiar, int edad, map<string, int> miembros)
+void registrar_miembros(string familiar, int edad, map<string, int> miembros)
 {
     cout << "Ingrese usted que mimbro de la familia es: " << endl;
     cin >> familiar;
@@ -25,19 +24,22 @@ registrar_miembros(string familiar, int edad, map<string, int> miembros)
     cout << "familiar registrado" << endl;
 }
 
-void mostrar_familiares(string miembro_familiar, string edad)
-{
-    cout << "Los miembros de la familia son: " << endl;
-    for (auto it = miembros.begin(); it != miembros.end(); it++)
-    {
-        cout << it->first << " " << it->second << endl;
-    }
-}
+// void integrangtes(map<string, int> miembros)
+// {
+//     cout << "los miembros de la familia son: " << endl;
+//     for (const auto &edades : edad)
+//         while (/* condition */)
+//         {
+//             /* code */
+//         }
+// }
 
 int main()
 {
     map<string, int> miembros;
     string miembro_familia;
-    string edad;
+    int edad;
+    // Miembros_familia *head;
     registrar_miembros(miembro_familia, edad, miembros);
+    // sortbyYear(head);
 }
